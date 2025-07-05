a4ca1a33de680876a36f8a972e1410cf64cead04
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-04 20:07:45 -0700

    updated build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/notes-app/",
+  base: "/LevelNotes/",
   build: {
     outDir: "../docs",
     emptyOutDir: true
